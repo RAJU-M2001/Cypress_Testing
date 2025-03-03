6de54bcc28063544a27206391ea830773b3b38b6
RAJU-M2001 <rajutamilarasan1@gmail.com>
2025-03-03 22:09:51 +0530

    commit

--- e2e/1-getting-started/Flipkart.cy.js
+++ e2e/1-getting-started/Flipkart.cy.js
@@ -1,7 +1,19 @@
 describe('Fundamental Testing',()=>{
     it('passes',() =>{
         cy.visit('https://www.flipkart.com/');
+        cy.xpath("//div[@class='H6-NpN _3N4_BX']").click();
+        cy.xpath("//input[@class='r4vIwl BV+Dqf']").type('9629113839');
+        cy.go('back');
+        cy.xpath("//span[@role='button']").click();
+        cy.xpath("//input[@class='Pke_EE']").type('JBL Headphone');
+        cy.xpath("//button[@class='_2iLD__']").click();
+        cy.scrollTo(0,2600);
+        cy.xpath("(//a[@class='wjcEIp'])[19]").click();
+        cy.xpath("//button[@class='QqFHMw vslbG+ _3Yl67G FMWyoY']").invoke
+
+
         
+
 
     });
 
