c6627b21d24a59166d475ed450b8fd315489b750
RAJU-M2001 <rajutamilarasan1@gmail.com>
2025-03-22 22:38:54 +0530

    Commit

--- e2e/1-getting-started/Upload_image.cy.js
+++ e2e/1-getting-started/Upload_image.cy.js
@@ -10,8 +10,8 @@ describe('Upload Image for profile', ()=> {
         // cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
 
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-        cy.get("input[placeholder='Username']").type('Admin');
-        cy.get("input[placeholder='Password']").type('admin123');
+        cy.get("input[placeholder='Username']").type('Admin')
+        cy.get("input[placeholder='Password']").type('admin123')
         cy.get("button[type='submit']").click();
         cy.wait(1000);
         cy.get("body > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > aside:nth-child(1) > nav:nth-child(1) > div:nth-child(2) > ul:nth-child(2) > li:nth-child(2) > a:nth-child(1) > span:nth-child(2)").click();
@@ -24,7 +24,11 @@ describe('Upload Image for profile', ()=> {
 //   .selectFile('cypress/fixtures/sample.png', { force: true });
 
          cy.get(".oxd-icon.bi-plus").click()
-         cy.get('input[type="file"]').selectFile('cypress/fixtures/sample.png', {force : true});
+         cy.get('input[type="file"]').selectFile('cypress/fixtures/20220210_230553.jpeg', {force : true});
+         cy.get(".oxd-switch-input.oxd-switch-input--active.--label-right").click();
+         cy.get("body > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > form:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div:nth-child(4) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > label:nth-child(1) > span:nth-child(2)").click();
+         cy.get(".oxd-main-menu-item.active").click();
+         cy.get("i[class='oxd-icon bi-caret-down-fill']").click();
     })
 
 });
